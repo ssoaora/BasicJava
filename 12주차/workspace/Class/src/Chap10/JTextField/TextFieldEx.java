7bb7a46cd0df5d76ba33e58448a67d49ef40dd3c
ssoaora <ssoaora@gmail.com>
2024-05-25 21:17:09 +0900

    :sparkles: Added: Week 12 Examples (JTextField)

---
+++ 12주차/workspace/Class/src/Chap10/JTextField/TextFieldEx.java
@@ -0,0 +1,32 @@
+package Chap10.JTextField;
+
+import java.awt.Container;
+import java.awt.FlowLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class TextFieldEx extends JFrame {
+
+	public TextFieldEx() {
+		this.setTitle("TextField 만들기 예제");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		Container cp = getContentPane();
+		cp.setLayout(new FlowLayout());
+
+		cp.add(new JLabel("이름   "));
+		cp.add(new JTextField(20));
+		cp.add(new JLabel("학과   "));
+		cp.add(new JTextField("컴퓨터공학과", 20));
+		cp.add(new JLabel("주소   "));
+		cp.add(new JTextField("서울시 ...", 20));
+
+		this.setSize(300, 200);
+		this.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new TextFieldEx();
+	}
+}
